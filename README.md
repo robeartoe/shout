f12061fbcf2ff5a3dc03e663f98aef6a6d8b936f
Robert Ruiz <rsblooxpert@gmail.com>
2020-05-26 20:57:43 -0700

    Updated project;

--- README.md
+++ README.md
@@ -46,7 +46,6 @@ Type the commands into your slack channel:
 
 ## Contributing:
 
-
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
-// import http = require('http');
-import NBA = require('nba');
+import NBA from 'nba';
+import express from 'express';
 
+// const express = require('express');
+const app = express();
 
 // TODO: Verify Webhook:
 
@@ -24,41 +26,49 @@ import NBA = require('nba');
  * @param {string} req.body.text The user's search query.
  * @param {object} res Cloud Function response object.
  */
-exports.ball = (req, res) => {
-    let params = req.body.params;
-    if (params === undefined){
+function ball(req: any, res: any): any {
+  let params = req.body.params;
+  if (params === undefined) {
 
-    }
-    else {
-        params = params.split(' ');
-    }
-    console.log(params);
+  } else {
+    params = params.split(' ');
+  }
+  console.log(params);
 
-    res.end(params);
+  res.end(params);
 
-    // Parse Through String:
-    if (params.indexOf(' ') !== -1) {
+  // Parse Through String:
+  if (params.indexOf(' ') !== -1) {
     // If we've got spaces, strip 'em
-        params = params.split(' ');
-    }
-    console.log(params);
+    params = params.split(' ');
+  }
+  console.log(params);
 
-    switch (params[0]) {
-        case 'team':
-            console.log('Team Called:');
-            break;
-        case 'player':
-            console.log('Player Called');
-            break;
-        default:
-            console.log('Incorrect Command!');
-            break;
+  switch (params[0]) {
+    case 'team':
+      console.log('Team Called:');
+      break;
+    case 'player':
+      console.log('Player Called');
+      break;
+    default:
+      console.log('Incorrect Command!');
+      break;
       // res.status(404).send('Incorrect command!');
-    }
-    // console.log(JSON.parse(data));
-    // res.write('Hello World!'); // write a response to the client
-    // res.end(); // end the response
-}; 
+  }
+  // console.log(JSON.parse(data));
+  // res.write('Hello World!'); // write a response to the client
+  // res.end(); // end the response
+}
+
+app.post('/', ball);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
+
 
 // const NBA = require('nba');
 // import 'nba';
