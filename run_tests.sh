9e34e84e622f9947b791480a49b8fc0b3b16cddb
Robert Ruiz <rsblooxpert@gmail.com>
2020-09-06 14:40:16 -0700

    integration tests working

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.3.2
 
-deno test test/
+deno test --allow-net test/main_test.ts
  
--- test/main_test.ts
+++ test/main_test.ts
@@ -1,58 +1,19 @@
-import app  from '../main.ts';
-import { superoak } from "https://deno.land/x/superoak@2.1.0/mod.ts";
-import {
-  assertEquals,
-  fail,
-} from 'https://deno.land/std/testing/asserts.ts';
-// import axiosd from 'https://deno.land/x/axiod/mod.ts';
+import {superoak} from 'https://deno.land/x/superoak@2.1.0/mod.ts';
 
-// const hostname = 'localhost';
-// const port = 8080;
-// const httpUrl = `http://${hostname}:${port}`;
-
-// const abortController: AbortController;
-
-// async function startServer() {
-//   abortController = new AbortController();
-//   console.log('Starting test server');
-//   try {
-//     listenPromise = new OakServer(hostname, port).start(abortController.signal);
-//     listenPromise = 
-//   } catch (err) {
-//     console.error(`Server can't start : ${err}`);
-//   }
-// }
-
-// function stopServer() {
-//   abortController.abort();
-//   await listenPromise;
-//   console.log('server stop');
-// }
-
-// function afterAll() {
-//   stopServer();
-// }
-
-// async function beforeAll() {
-//   await startServer();
-// }
-
-Deno.test('test', async () => {
-  const test = 1 + 3;
-  assertEquals(test, 4);
+Deno.test('Ensure only POST methods is allowed', async () => {
+  const request = await superoak('http://localhost:8080');
+  await request.get('/').expect(405);
+  await request.put('/').expect(405);
+  await request.delete('/').expect(405);
 });
 
-// Deno.test('Get all user', async () => {
-//   await beforeAll();
-//   const response = await axiosd.get(`${httpUrl}/users`);
-//   const users = response.data.data;
+Deno.test('Should return 400 if no body is provided', async () => {
+  const request = await superoak('http://localhost:8080');
+  await request.post('/').expect(400);
+});
 
-//   assertEquals(response.status, 200);
-//   assertEquals(users.length, 2);
-//   assertEquals(users[0]['id'], '1');
-//   assertEquals(users[0]['name'], 'Bob');
-//   assertEquals(users[1]['id'], '2');
-//   assertEquals(users[1]['name'], 'Alice');
 
-//   afterAll();
+// Deno.test('Shout Service Various Cases:', async () => {
+//   const request = await superoak('http://localhost:8080');
+//   await request.post('/');
 // });
