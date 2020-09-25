e285a364e7833d9b9b7711c30e396d079304553e
Robert Ruiz <rsblooxpert@gmail.com>
2020-09-25 08:37:40 -0700

    Switching initial & final main.ts files

--- README.md
+++ README.md
@@ -6,31 +6,43 @@ Clone this repository
 ```bash 
 git clone https://github.com/robeartoe/shout.git
 ```
-Using the config.default.json file as a template, make a config.json file with your slack token:
-```
-{
-    "SLACK_TOKEN" : "YOUR_SLACK_TOKEN"
-}
+
+## Running Locally:
+To run locally make sure you have Docker installed, and an API explorer like Insomnia or Postman. The only request is a POST request.
+```bash
+# Build the application:
+docker build --tag shout:0.1 .
+# Run the containerized application in the background.
+# Expose Port 8080, from within the application, and outside the application.
+# Meaning we could access the application from http://localhost:8080
+docker run --publish 8080:8080 --detach --name shout shout:0.1
 ```
 
-## Development:
-<!-- TODO: Redo this section for Cloud Run -->
-
-Afterwards you can send request to that function instance.
-You can send the commands 
+When you are done, remove the docker container, and stop it from running:
+```bash
+docker rm --force shout
 ```
+
+Afterwards you can send request to that application.
+You can send the JSON - POST request:
+```json
 {
-	"params": "test"
+	"text": "test"
 }
 ```
 
 ## Deployment:
 To deploy the function, make sure gcloud is installed.
+Also know what your GCP project ID is, because that is where we are sending our code to be containerized & deployed.
 ```bash
- #TODO: Build command / script!
+ gcloud builds submit --tag gcr.io/${YOUR GCP Project ID}/shout
+ gcloud run deploy --image gcr.io/${YOUR GCP Project ID}/shout --platform managed
 ```
 
 ## Usage:
+Instal the application onto your slack channel.
+Link the slack command to the cloud run URL to send commands to.
+Then you should be good to go!
 Type the commands into your slack channel:
 ```
 /shout see? I'm really passionate about this topic!
--- main_final.ts
+++ main_final.ts
@@ -3,26 +3,43 @@ import {Application, Router} from 'https://deno.land/x/oak@v6.0.1/mod.ts';
 import bodyParser from './body-parser.ts';
 import 'https://deno.land/x/dotenv/mod.ts';
 
+interface SlackRequest {
+  token:string;
+  team_id:string;
+  team_domain:string;
+  enterprise_id:string;
+  enterprise_name:string;
+  channel_id:string;
+  channel_name:string;
+  user_id:string;
+  user_name:string;
+  command: string;
+  text:string;
+  response_url:string;
+  trigger_id:string;
+  api_app_id:string;
+}
+
 const app = new Application();
 const PORT = Deno?.env?.get('PORT') ?? 8080;
 const router = new Router();
 
 /**
- * Submit text to be shouted
+ * Receive a Slash Command request from Slack.
  *
  * @example
- * curl -X POST "https://localhost:8080/" --data '{"text":"TEST"}'
+ * curl -X POST "https://shout-vmh6lxa4qa-uw.a.run.app/" --data '{"text":"TEST", ...rest of slack information}'
+ * More information on slack information that is sent can be found here:
  * https://api.slack.com/interactivity/slash-commands#app_command_handling
- * @param {object} req request object.
+ * @param {object} req Cloud Function request object.
  * @param {object} req.body The request payload.
  * @param {string} req.body.text The user's text.
- * @param {object} res response object.
+ * @param {object} res Cloud Function response object.
  */
 router
     .post('/', async (context) => {
       try {
         // Get the Body:
-        const body = context.request.body();
         if (!context.request.hasBody) {
           context.response.status = 400;
           context.response.body = {
@@ -31,13 +48,17 @@ router
           };
           return;
         }
-        const json = await context.request.body({type: "json"}).value;
-        const result = await FetchShoutCloud(json.text! as string);
-
+        const {text, user_name}: SlackRequest = await bodyParser(context, ['text', 'user_name', 'team_id', 'channel_name', 'command']);
+        const regex = /\+/gi;
+        const parsedText = text.split('&')[0].replace(regex, ' ');
+        // Call ShoutCloud:
+        const result = await FetchShoutCloud(parsedText as string);
+        // Respond back to Slack:
         context.response.status = 200;
         context.response.type = 'application/json';
         context.response.body = {
-          'text': `${result}`,
+          'response_type': 'in_channel',
+          'text': `${BuildIntro(user_name)}${result}`,
         };
         return;
       } catch (error) {
@@ -46,6 +67,21 @@ router
         return;
       }
     });
+
+function BuildIntro(user_name: string): string {
+  function GetRandomInt(max: number) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const intros: string[] = [
+    `Umm, did you hear? ${user_name} just said.. `,
+    `What part didn't you understand? ${user_name} is trying to say.. `,
+    `DID ${user_name.toUpperCase()} STUTTER? ${user_name.toUpperCase()} SAID `,
+    `${user_name} just said `,
+    `What ${user_name} is trying to say is `,
+  ];
+  return intros[GetRandomInt(intros.length)];
+}
 
 async function FetchShoutCloud(command: string): Promise<string> {
   const data = await (await fetch('HTTP://API.SHOUTCLOUD.IO/V1/SHOUT', {
